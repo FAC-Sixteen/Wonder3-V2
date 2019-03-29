fa689e457d3f2383b6dc5250984dd0e1b9e6f4aa
Sylvia <35779506+seabasshoang@users.noreply.github.com>
2019-03-29 11:27:25 +0000

    add process.env.PORT

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const router = require('./router');
-const port = 4001;
+// const port = 4001;
 // const matcher = require('./script.js')
 
 const server = http.createServer(router);
 
-server.listen(port);
+server.listen(process.env.PORT || 4001);
 
 console.log('Server running on port 4001');
 
